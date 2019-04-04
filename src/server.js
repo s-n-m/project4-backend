0a3f1b87b4979b4f3005393c04bfefb9315cd96e
Mohammed <4m.4s.2013@gmail.com>
2019-04-04 10:47:51 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -22,8 +22,14 @@ const app = express();
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 
+const corsOptions = {
+  origin: '*',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 // define port for API to run on
 const port = process.env.PORT;
 
